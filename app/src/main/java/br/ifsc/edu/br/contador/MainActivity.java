5357a97cfa91d5e9805b2e39e965252f1c8b5063
Augusto Reis <devaugustoreis@gmail.com>
2024-09-09 22:04:10 -0300

    Numero aleatorio

--- app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button myBtn = findViewById(R.id.button);
+        TextView myText = findViewById(R.id.textView);
+        EditText editTextInicial = findViewById(R.id.editText1);
+        EditText editTextFinal = findViewById(R.id.editText2);
         myBtn.setText("blah");
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button b = (Button) view;
+                // Button b = (Button) view;
                 contador++;
-                b.setText((int) Math.random());
+                Random random = new Random();
+                int nInicial = Integer.parseInt(editTextInicial.getText().toString());
+                int nFinal = Integer.parseInt(editTextFinal.getText().toString());
+                myText.setText(Integer.toString(random.nextInt(nFinal + 1 - nInicial) + nInicial));
             }
         });
     }
